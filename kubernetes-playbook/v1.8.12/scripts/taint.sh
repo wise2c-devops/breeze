5672bac0ecc9112aa172930947d8494d10003db6
Alan Peng <peng.alan@gmail.com>
2018-05-10 01:33:32 -0400

    Updated kubernetes version

--- kubernetes-playbook/v1.8.12/scripts/taint.sh
+++
@@ -1,8 +0,0 @@
-HOSTNAME=`hostname`
-while : ; do
-  kubectl taint --overwrite nodes ${HOSTNAME,,} node-role.kubernetes.io=master:NoSchedule
-  if [ $? != 0 ]; then
-    sleep 2
-  else break
-  fi
-done
