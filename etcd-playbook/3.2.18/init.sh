ca33441b1b7d87ce736ef58c81f57a8c16aa2c65
Alan Peng <peng.alan@gmail.com>
2018-08-23 11:09:27 +0800

    Update init.sh

--- etcd-playbook/3.2.18/init.sh
+++ etcd-playbook/3.2.18/init.sh
@@ -4,7 +4,7 @@ set -e
 
 path=`dirname $0`
 
-image=gcr.io/etcd-amd64:${1}
+image=k8s.gcr.io/etcd-amd64:${1}
 echo "" >> ${path}/group_vars/etcd.yml
 echo "image: ${image}" >> ${path}/group_vars/etcd.yml
 
