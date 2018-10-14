0d258e6be05b7ff1ed11db2d843589f14717db61
Alan Peng <peng.alan@gmail.com>
2018-10-15 01:08:01 +0800

    update playbook for v1.11.3

--- init.sh
+++ init.sh
@@ -4,6 +4,11 @@ set -e
 
 path=`dirname $0`
 
+docker run --rm --name=kubeadm-version wise2ck8s/kubeadm-version:$TRAVIS_BRANCH kubeadm config images list > ${path}/k8s-images-list.txt
+
+etcd_version=`cat ${path}/k8s-images-list.txt |grep etcd |awk -F ':' '{print $2}'`
+mv etcd-playbook/version-by-kubeadm etcd-playbook/${etcd_version}
+
 for dir in `ls ${path}`
 do
     if [[ ${dir} =~ -playbook$ ]]; then
