e28f7e310bc55e06458da3eacdfa904dab905a2a
Alan Peng <peng.alan@gmail.com>
2018-10-28 19:40:25 +0800

    100 years for CA and 50 years for other certificates & Upgrade to Kubernetes v1.12.2

--- kubernetes-playbook/v1.12.2/init.sh
+++ kubernetes-playbook/v1.12.2/init.sh
@@ -4,7 +4,7 @@ set -e
 
 path=`dirname $0`
 
-docker run --rm --name=kubeadm-version wise2c/kubeadm-version:$TRAVIS_BRANCH kubeadm config images list  --kubernetes-version 1.12.1 > ${path}/k8s-images-list.txt
+docker run --rm --name=kubeadm-version wise2c/kubeadm-version:$TRAVIS_BRANCH kubeadm config images list  --kubernetes-version 1.12.2 > ${path}/k8s-images-list.txt
 
 echo "=== pulling kubernetes images ==="
 for IMAGES in $(cat ${path}/k8s-images-list.txt |grep -v etcd); do
@@ -74,3 +74,12 @@ docker save ${dashboard_repo}/kubernetes-dashboard-amd64:${dashboard_version} \
 rm ${path}/file/dashboard.tar.bz2 -f
 bzip2 -z --best ${path}/file/dashboard.tar
 echo "=== kubernetes dashboard image is saved successfully ==="
+
+echo "=== download cfssl tools ==="
+export CFSSL_URL=https://pkg.cfssl.org/R1.2
+curl -L -o cfssl ${CFSSL_URL}/cfssl_linux-amd64
+curl -L -o cfssljson ${CFSSL_URL}/cfssljson_linux-amd64
+curl -L -o cfssl-certinfo ${CFSSL_URL}/cfssl-certinfo_linux-amd64
+chmod +x cfssl cfssljson cfssl-certinfo
+tar zcvf ${path}/file/cfssl-tools.tar.gz cfssl cfssl-certinfo cfssljson
+echo "=== cfssl tools is download successfully ==="
