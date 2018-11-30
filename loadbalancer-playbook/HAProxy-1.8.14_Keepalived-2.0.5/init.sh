971868e9766a35e551c99511f3c03c31a1228c12
Alan Peng <peng.alan@gmail.com>
2018-11-30 12:48:40 -0500

    Update to k8s 1.12.3 and add HA UI components

--- loadbalancer-playbook/HAProxy-1.8.14_Keepalived-2.0.5/init.sh
+++ loadbalancer-playbook/HAProxy-1.8.14_Keepalived-2.0.5/init.sh
@@ -7,13 +7,11 @@ echo "build wise2c/k8s-keepalived:2.0.5 image"
 cd ${path}/keepalived
 docker build -t wise2c/k8s-keepalived:2.0.5 .
 #docker tag wise2c/keepalived-k8s wise2c/k8s-keepalived:2.0.5
-docker save wise2c/k8s-keepalived:2.0.5 -o keepalived-2.0.5.tar
-bzip2 -z --best keepalived-2.0.5.tar
-mv keepalived-2.0.5.tar.bz2 ${path}/file/
+docker save wise2c/k8s-keepalived:2.0.5 -o ${path}/keepalived-2.0.5.tar
+bzip2 -z --best ${path}/keepalived-2.0.5.tar
 
 echo "build wise2c/k8s-haproxy:1.8.14 image"
 docker pull haproxy:1.8.14
 docker tag haproxy:1.8.14 wise2c/k8s-haproxy:1.8.14
-docker save wise2c/k8s-haproxy:1.8.14 -o haproxy-1.8.14.tar
-bzip2 -z --best haproxy-1.8.14.tar
-mv haproxy-1.8.14.tar.bz2 ${path}/file/
+docker save wise2c/k8s-haproxy:1.8.14 -o ${path}/haproxy-1.8.14.tar
+bzip2 -z --best ${path}/haproxy-1.8.14.tar
