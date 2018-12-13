50c52854abfb9a4a06ba360a23e58f1dbccbbc1b
Alan Peng <peng.alan@gmail.com>
2018-12-13 23:17:02 +0800

    Update generate-apiserver-certificate.sh

--- kubernetes-playbook/v1.13.0/file/generate-apiserver-certificate.sh
+++ kubernetes-playbook/v1.13.0/file/generate-apiserver-certificate.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 # Get host IP address and hostname
-HOST_IP={{ ansible_default_ipv4.address }}
-HOST_NAME=$(hostname)
+WISE2C_IP_LABEL=$(cat /etc/hosts |grep -A 1 'BEGIN WISE2C DEPLOY MANAGED BLOCK' |grep -v '#' |grep -v '^\-\-' |wc |awk '{print $1}')
+
+if [ "${WISE2C_IP_LABEL}" = "0" ]; then
+  HOST_IP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
+  HOST_NAME=$(hostname)
+else
+  for IP_Addresses in $(cat /etc/hosts |grep -A 1 'BEGIN WISE2C DEPLOY MANAGED BLOCK' |grep -v '#' |grep -v '^\-\-' |awk '{print $1}');
+  do
+    GrepStr=$(ip a |grep "inet $IP_Addresses")
+    if [ -n "$GrepStr" ]; then
+      HOST_IP=$IP_Addresses
+      HOST_NAME=$(cat /etc/hosts |grep -A 1 'BEGIN WISE2C DEPLOY MANAGED BLOCK' |grep -v '#' |grep -v '^\-\-' |grep $HOST_IP |awk '{print $2}')
+    fi
+  done;
+fi
 
 HOST_VIP=`cat /var/tmp/wise2c/kubernetes/kubeadm.conf | grep -A 1 SAN | tail -1 | awk '{print $2}'`
 
