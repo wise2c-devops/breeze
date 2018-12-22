15c36d6b55c5d09071e7196f2c0b8e890f0a8401
Alan Peng <peng.alan@gmail.com>
2018-12-22 07:35:02 -0500

    Updated components version

--- harbor-playbook/version/init.sh
+++ harbor-playbook/version/init.sh
@@ -3,14 +3,16 @@
 set -e
 
 path=`dirname $0`
-version=$1
+
+version=`cat ${path}/components-version.txt |grep "Harbor" |awk '{print $3}'`
+
 echo "" >> ${path}/yat/harbor.yml.gotmpl
-echo "version: ${version}" >> ${path}/yat/harbor.yml.gotmpl
+echo "version: v${version}" >> ${path}/yat/harbor.yml.gotmpl
 
-curl -L https://storage.googleapis.com/harbor-releases/harbor-offline-installer-${version}.tgz \
-    -o ${path}/file/harbor-offline-installer-${version}.tgz
+curl -L https://storage.googleapis.com/harbor-releases/release-${version}/harbor-offline-installer-v${version}.tgz \
+    -o ${path}/file/harbor-offline-installer-v${version}.tgz
 
-curl -sSL https://raw.githubusercontent.com/vmware/harbor/${version}/make/harbor.cfg \
+curl -sSL https://raw.githubusercontent.com/vmware/harbor/v${version}/make/harbor.cfg \
     | sed \
     -e "s,hostname = reg\.mydomain\.com,hostname = {{ inventory_hostname }},g" \
     -e "s,harbor_admin_password = Harbor12345,harbor_admin_password = {{ password }},g" \
