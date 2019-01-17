0bc5f35ac168a9103d381f91116d2f274e8f1b12
Alan Peng <peng.alan@gmail.com>
2019-01-17 13:20:45 +0800

    Supports multi-NIC

---
+++ kubernetes-playbook/version/file/update-api-advertise-address.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set +e
+
+# Get host IP address
+WISE2C_IP_LABEL=$(cat /etc/hosts |grep -A 1 'BEGIN WISE2C DEPLOY MANAGED BLOCK' |grep -v '#' |grep -v '^\-\-' |wc |awk '{print $1}')
+
+if [ "${WISE2C_IP_LABEL}" = "0" ]; then
+  HOST_IP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
+else
+  for IP_Addresses in $(cat /etc/hosts |grep -A 1 'BEGIN WISE2C DEPLOY MANAGED BLOCK' |grep -v '#' |grep -v '^\-\-' |awk '{print $1}');
+  do
+    GrepStr=$(ip a |grep "inet $IP_Addresses")
+    if [ -n "$GrepStr" ]; then
+      HOST_IP=$IP_Addresses
+    fi
+  done;
+fi
+
+sed -i "s/advertiseAddress: 127.0.0.1/advertiseAddress: ${HOST_IP}/g" kubeadm.conf
