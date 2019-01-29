904b205f51058baf652f4e4d62b364cde6022dfc
HYmian <gopher.mian@outlook.com>
2019-01-29 21:48:59 +0800

    Merge pull request #51 from wise2c-devops/check-env
    Check env

---
+++ docker-playbook/version/scripts/check_environment.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+set -e
+
+function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
+
+[ ${BREEZE_LSB_ID} ]
+[ ${BREEZE_LSB_RELEASE} ]
+#[ ${BREEZE_KERNEL} ]
+[ ${BREEZE_PYTHON_VERSION} ]
+
+if [ "${BREEZE_LSB_ID}" != "CentOS" ] && [ "${BREEZE_LSB_ID}" != "Ubuntu" ]; then
+  echo "please use CentOS or Ubuntu"
+  exit
+fi
+
+if [ "${BREEZE_LSB_ID}" == "CentOS" ] && [ `version_gt 7.3 ${BREEZE_LSB_RELEASE}` ]; then
+  echo "please use CentOS 7.4/7.5/7.6 for Breeze"
+  exit
+fi
+
+# TODO: complete unbuntu
+if [ "${BREEZE_LSB_ID}" == "Ubuntu" ] && [ `version_gt 16 ${BREEZE_LSB_RELEASE}` ]; then
+  echo "please use Ubuntu 16 for Breeze"
+  exit
+fi
+
+if [ `version_gt 2.7 ${BREEZE_PYTHON_VERSION}` ]; then
+  echo "please use python 2.7+"
+  exit
+fi
+
+printf true
