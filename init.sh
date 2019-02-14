7941cd403cbf597942d53ec80abf4bb3d15db854
Alan Peng <peng.alan@gmail.com>
2019-02-14 18:46:33 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ path=`dirname $0`
 
 kubernetes_version=1.12.5
 harbor_version=1.7.2
-docker_version=18.06.1
+docker_version=18.06.2
 haproxy_version=1.8.14
 keepalived_version=1.3.5
 loadbalancer_version=HAProxy-${haproxy_version}_Keepalived-${keepalived_version}
