4c67ed36fffa656b4e6ffa9e17380d7d92cede73
Alan Peng <peng.alan@gmail.com>
2019-04-05 09:19:36 +0800

    Update Kubernetes to v1.12.7

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ set -e
 
 path=`dirname $0`
 
-kubernetes_version=1.12.6
-harbor_version=1.7.3
+kubernetes_version=1.12.7
+harbor_version=1.7.5
 docker_version=18.06.3
 haproxy_version=1.8.14
 keepalived_version=1.3.5
