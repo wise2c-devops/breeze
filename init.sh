0032e92906abdda79e3a8ae65c8db60b9c8127fc
Alan Peng <peng.alan@gmail.com>
2019-04-05 09:20:48 +0800

    Update Kubernetes to v1.13.5

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ set -e
 
 path=`dirname $0`
 
-kubernetes_version=1.13.4
-harbor_version=1.7.3
+kubernetes_version=1.13.5
+harbor_version=1.7.5
 docker_version=18.06.3
 haproxy_version=1.8.14
 keepalived_version=1.3.5
