1a3a122e67e64b2bc391833c9b1c45389d267fbd
Alan Peng <peng.alan@gmail.com>
2019-06-06 02:07:12 +0800

    Support etcd upgrading

--- etcd-playbook/version-by-kubeadm/file/backup-etcd-database-folder.sh
+++ etcd-playbook/version-by-kubeadm/file/backup-etcd-database-folder.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-set -e
-
-etcdurl=`docker exec etcd ps -o args |grep -v ps |grep -v COMMAND |awk '{print $7}'`
-health=`docker exec etcd etcdctl --endpoints ${etcdurl} cluster-health |grep 'cluster is healthy'`
-if [ "$health" = "cluster is healthy" ]; then
-  echo cluster is healthy
-  cp -pr /data/etcd /data/etcd-backup-`date +%F-%H-%M-%S`
-else
-  echo cluster is not healthy
-  exit 1
-fi
+cp -pr /data/etcd /data/etcd-backup-`date +%F-%H-%M-%S`
