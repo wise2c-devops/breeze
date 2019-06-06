af32da919e9c1d490a7f3b4f6376c6762f53d9a7
Alan Peng <peng.alan@gmail.com>
2019-06-06 12:58:33 +0800

    Support etcd upgrading

---
+++ etcd-playbook/version-by-kubeadm/file/make-etcd-snapshot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+etcdurl=`docker exec etcd ps -o args |grep -v ps |grep -v COMMAND |awk '{print $7}'`
+health=`docker exec etcd etcdctl --endpoints ${etcdurl} cluster-health |grep 'cluster is healthy'`
+if [ "$health" = "cluster is healthy" ]; then
+  echo cluster is healthy
+  backuptime=`date +%F-%H-%M-%S`
+  etcdurl=`docker exec etcd ps -o args |grep -v ps |grep -v COMMAND |awk '{print $7}'`
+  etcdbackup=`docker exec -e ETCDCTL_API=3 etcd etcdctl --endpoints ${etcdurl} snapshot save snapshotdb-${backuptime} |awk '{print $4}'`
+  docker cp etcd:/${etcdbackup} ./
+  docker exec -it etcd rm -f /${etcdbackup}
+else
+  echo cluster is not healthy
+  exit 1
+fi
