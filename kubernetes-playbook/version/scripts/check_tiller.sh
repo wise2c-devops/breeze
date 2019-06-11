526964ff2906235befe0d4921037ca1d2683550f
Alan Peng <peng.alan@gmail.com>
2019-06-11 21:28:46 +0800

    Update helm init script

--- kubernetes-playbook/version/scripts/check_tiller.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-tiller_status="false"
-until [ "$tiller_status" = "Available" ]; do sleep 1; printf "."; tiller_status=`kubectl -n kube-system get deploy tiller-deploy -o jsonpath='{.status.conditions[0].type}' 2>&1`; done
-sleep 10
