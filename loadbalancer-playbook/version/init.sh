fc33b6561c47d7eb4e599af989df8bc2bc24bfe5
Alan Peng <peng.alan@gmail.com>
2019-06-15 16:45:45 +0800

    Update loadbalancer version

--- loadbalancer-playbook/version/init.sh
+++ loadbalancer-playbook/version/init.sh
@@ -5,6 +5,9 @@ path=`dirname $0`
 haproxy_version=`cat ${path}/components-version.txt |grep "HAProxy" |awk '{print $3}'`
 keepalived_version=`cat ${path}/components-version.txt |grep "Keepalived" |awk '{print $3}'`
 
+echo "haproxy_version: ${haproxy_version}" > ${path}/yat/loadbalancer.yml.gotmpl
+echo "keepalived_version: ${keepalived_version}" >> ${path}/yat/loadbalancer.yml.gotmpl
+
 echo "build wise2c/k8s-keepalived:${keepalived_version} image"
 cd ${path}/keepalived
 docker build -t wise2c/k8s-keepalived:${keepalived_version} .
@@ -17,6 +20,6 @@ docker tag haproxy:${haproxy_version} wise2c/k8s-haproxy:${haproxy_version}
 docker save wise2c/k8s-haproxy:${haproxy_version} -o ../file/haproxy-${haproxy_version}.tar
 bzip2 -z --best ../file/haproxy-${haproxy_version}.tar
 
-sed -i "s/1.8.14/${haproxy_version}/g" ../template/haproxy-run.sh.j2
-sed -i "s/1.8.14/${haproxy_version}/g" ../install.ansible
-sed -i "s/1.3.5/${keepalived_version}/g" ../install.ansible
+sed -i "s/HAPROXY_VERSION/${haproxy_version}/g" ../template/haproxy-run.sh.j2
+sed -i "s/HAPROXY_VERSION/${haproxy_version}/g" ../install.ansible
+sed -i "s/KEEPALIVED_VERSION/${keepalived_version}/g" ../install.ansible
