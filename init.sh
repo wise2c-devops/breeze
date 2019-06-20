c1eb6e2dfcbee88c51f5357be97e8e3894a369a3
Alan Peng <peng.alan@gmail.com>
2019-06-20 10:56:35 +0800

    Update HAProxy Harbor and Istio

--- init.sh
+++ init.sh
@@ -5,9 +5,9 @@ set -e
 path=`dirname $0`
 
 kubernetes_version=1.14.3
-harbor_version=1.8.0
+harbor_version=1.8.1
 docker_version=18.09.6
-haproxy_version=1.9.8
+haproxy_version=2.0.0
 keepalived_version=1.3.5
 loadbalancer_version=HAProxy-${haproxy_version}_Keepalived-${keepalived_version}
 prometheus_version=2.7.2
@@ -17,7 +17,7 @@ metrics_server_version=0.3.3
 dashboard_version=1.10.1
 flannel_version=0.11.0
 helm_version=2.14.1
-istio_version=1.1.8
+istio_version=1.2.0
 
 mv ${path}/kubernetes-playbook/version ${path}/kubernetes-playbook/v${kubernetes_version}
 mv ${path}/harbor-playbook/version ${path}/harbor-playbook/v${harbor_version}
