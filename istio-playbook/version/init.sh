e36e6faae900fe5900a0da51d1bb1f3500cb66f9
Alan Peng <peng.alan@gmail.com>
2019-06-20 08:30:21 +0800

    Add Istio Playbook

--- istio-playbook/version/init.sh
+++ istio-playbook/version/init.sh
@@ -16,7 +16,7 @@ tar zxf istio-$IstioVersion-origin.tar.gz
 cat istio-$IstioVersion/install/kubernetes/istio-demo.yaml |grep "image:" |grep -v '\[\[' |grep -v '{' |awk -F':' '{print $2":"$3}' |awk -F "[\"\"]" '{print $2}' |awk '!a[$0]++{print}' > images-list.txt
 
 echo 'Images list for Istio:'
-cat $images-list.txt
+cat images-list.txt
 
 for file in $(cat images-list.txt); do docker pull $file; done
 echo 'Images pulled.'
