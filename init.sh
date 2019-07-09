08bfd49e4f380958543b4b7ef27b1e64c306ab6d
Alan Peng <peng.alan@gmail.com>
2019-07-09 15:55:02 +0800

    Update Kubernetes to v1.12.10

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ set -e
 
 path=`dirname $0`
 
-kubernetes_version=1.12.9
+kubernetes_version=1.12.10
 harbor_version=1.8.1
 docker_version=18.06.3
 haproxy_version=2.0.0
@@ -17,7 +17,7 @@ metrics_server_version=0.3.3
 dashboard_version=1.10.1
 flannel_version=0.11.0
 helm_version=2.14.1
-istio_version=1.2.0
+istio_version=1.2.2
 
 mv ${path}/kubernetes-playbook/version ${path}/kubernetes-playbook/v${kubernetes_version}
 mv ${path}/harbor-playbook/version ${path}/harbor-playbook/v${harbor_version}
