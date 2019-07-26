d721cca9ea79f740748cae292a22394c19ecba0d
Alan Peng <peng.alan@gmail.com>
2019-07-26 14:12:55 +0800

    Supports Ubuntu 16/18 OS

---
+++ kubernetes-playbook/version/file/prometheus-fix-master-nodes-ubuntu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+KUBEADM_SYSTEMD_CONF=/etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+#KUBEADM_SYSTEMD_CONF=/usr/lib/systemd/system/kubelet.service.d/10-kubeadm.conf
+sed -e "/cadvisor-port=0/d" -i "$KUBEADM_SYSTEMD_CONF"
+if ! grep -q "authentication-token-webhook=true" "$KUBEADM_SYSTEMD_CONF"; then
+  sed -e "s/--authorization-mode=Webhook/--authentication-token-webhook=true --authorization-mode=Webhook/" -i "$KUBEADM_SYSTEMD_CONF"
+fi
+
+# Only for kubeadm setup
+sed -e "s/- --address=127.0.0.1/- --address=0.0.0.0/" -i /etc/kubernetes/manifests/kube-controller-manager.yaml
+sed -e "s/- --address=127.0.0.1/- --address=0.0.0.0/" -i /etc/kubernetes/manifests/kube-scheduler.yaml
+
+systemctl daemon-reload
+systemctl restart kubelet
