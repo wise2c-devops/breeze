a02b7e9177a5fdb6661ae9ea7370564fe0170863
Alan Peng <peng.alan@gmail.com>
2019-07-26 13:54:19 +0800

    Supports Ubuntu 16/18 OS

--- docker-playbook/version/scripts/check_environment.sh
+++ docker-playbook/version/scripts/check_environment.sh
@@ -3,34 +3,30 @@
 set -e
 
 function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
-function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -V -r | head -n 1)" != "$1"; }
 
 [ ${BREEZE_LSB_ID} ]
 [ ${BREEZE_LSB_RELEASE} ]
+#[ ${BREEZE_KERNEL} ]
 [ ${BREEZE_PYTHON_VERSION} ]
 
-if [ "${BREEZE_LSB_ID}" != "RedHat" ] && [ "${BREEZE_LSB_ID}" != "CentOS" ] && [ "${BREEZE_LSB_ID}" != "Ubuntu" ]; then
-  echo "Breeze currently only supports RedHat, CentOS and Ubuntu."
+if [ "${BREEZE_LSB_ID}" != "CentOS" ] && [ "${BREEZE_LSB_ID}" != "Ubuntu" ]; then
+  echo "please use CentOS or Ubuntu"
   exit
 fi
 
-if [ "${BREEZE_LSB_ID}" == "RedHat" ] && [ `version_lt 7.4 ${BREEZE_LSB_RELEASE}` ]; then
-  echo "Breeze currently only supports RedHat/CentOS 7.4+ and Ubuntu 16/18."
+if [ "${BREEZE_LSB_ID}" == "CentOS" ] && [ `version_gt 7.3 ${BREEZE_LSB_RELEASE}` ]; then
+  echo "please use CentOS 7.4/7.5/7.6 for Breeze"
   exit
 fi
 
-if [ "${BREEZE_LSB_ID}" == "CentOS" ] && [ `version_lt 7.4 ${BREEZE_LSB_RELEASE}` ]; then
-  echo "Breeze currently only supports RedHat/CentOS 7.4+ and Ubuntu 16/18."
+# TODO: complete unbuntu
+if [ "${BREEZE_LSB_ID}" == "Ubuntu" ] && [ `version_gt 16 ${BREEZE_LSB_RELEASE}` ]; then
+  echo "please use Ubuntu 16 for Breeze"
   exit
 fi
 
-if [ "${BREEZE_LSB_ID}" == "Ubuntu" ] && [ `version_lt 16 ${BREEZE_LSB_RELEASE}` ]; then
-  echo "Breeze currently only supports RedHat/CentOS 7.4+ and Ubuntu 16/18."
-  exit
-fi
-
-if [ `version_lt 2.7 ${BREEZE_PYTHON_VERSION}` ]; then
-  echo "Breeze currently only supports python 2.7+"
+if [ `version_gt 2.7 ${BREEZE_PYTHON_VERSION}` ]; then
+  echo "please use python 2.7+"
   exit
 fi
 
