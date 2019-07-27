318e9b2a69567dcfb3081c03adca5fa3a26982a9
Alan Peng <peng.alan@gmail.com>
2019-07-28 00:55:37 +0800

    Supports Ubuntu 16/18 OS

--- prometheus-playbook/version/file/deploy.sh
+++ prometheus-playbook/version/file/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+#If seems that there is a bug on Ubuntu host to load the images. If no wait, it will return an error message: "Error response from daemon: No such image"
+sleep 60
+
 MyImageRepositoryIP=`cat harbor-address.txt`
 MyImageRepositoryProject=library
 KubePrometheusVersion=`cat components-version.txt |grep "KubePrometheus" |awk '{print $3}'`
