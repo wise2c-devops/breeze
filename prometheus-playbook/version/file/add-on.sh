ebee99ca6e138a673fc7e7dd3ced74400ba26072
Alan Peng <peng.alan@gmail.com>
2019-07-31 01:49:50 +0800

    Add certificate for etcd cluster

--- prometheus-playbook/version/file/add-on.sh
+++ prometheus-playbook/version/file/add-on.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+KubePrometheusVersion=`cat components-version.txt |grep "KubePrometheus" |awk '{print $3}'`
+
 kubectl apply -f kube-controller-manager.yaml
 kubectl apply -f kube-scheduler.yaml
 kubectl apply -f coredns.yaml
@@ -10,5 +12,14 @@ etcd3_address=`cat etcd-address.txt |awk -F "," '{print $3}' |awk -F "//" '{prin
 sed -i "s/etcd_1_address/${etcd1_address}/g" /var/tmp/wise2c/prometheus/etcd.yaml
 sed -i "s/etcd_2_address/${etcd2_address}/g" /var/tmp/wise2c/prometheus/etcd.yaml
 sed -i "s/etcd_3_address/${etcd3_address}/g" /var/tmp/wise2c/prometheus/etcd.yaml
+
+kubectl -n monitoring create secret generic etcd-certs --from-file=/etc/etcd/pki/ca.pem --from-file=/etc/etcd/pki/etcd.pem --from-file=/etc/etcd/pki/etcd-key.pem
+
+cat >> kube-prometheus-$KubePrometheusVersion/manifests/phase2/prometheus-prometheus.yaml << EOF
+  secrets:
+  - etcd-certs
+EOF
+
+kubectl -n monitoring apply -f kube-prometheus-$KubePrometheusVersion/manifests/phase2/prometheus-prometheus.yaml
 
 kubectl -n monitoring apply -f etcd.yaml
