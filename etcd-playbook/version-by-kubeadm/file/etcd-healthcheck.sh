1237441a264558424cd06f69a327a297ab898eec
Alan Peng <peng.alan@gmail.com>
2019-08-07 22:26:12 +0800

    Update etcd-healthcheck.sh

--- etcd-playbook/version-by-kubeadm/file/etcd-healthcheck.sh
+++ etcd-playbook/version-by-kubeadm/file/etcd-healthcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-etcdurl=`docker exec etcd ps -o args |grep -v ps |grep -v COMMAND |awk '{print $7}'`
+etcdurl=`docker exec etcd ps -o args |grep -v args |grep -v COMMAND |awk '{print $7}'`
 docker exec etcd etcdctl --endpoints ${etcdurl} cluster-health |grep 'cluster is healthy'
