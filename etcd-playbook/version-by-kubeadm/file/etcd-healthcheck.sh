7d1fc0144f41246d31ca7b70261b9283b00a28b7
Alan Peng <peng.alan@gmail.com>
2019-08-07 22:27:29 +0800

    Update etcd-healthcheck.sh

--- etcd-playbook/version-by-kubeadm/file/etcd-healthcheck.sh
+++ etcd-playbook/version-by-kubeadm/file/etcd-healthcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-etcdurl=`docker exec etcd ps -o args |grep -v COMMAND |awk '{print $7}'`
+etcdurl=`docker exec etcd ps -o args |grep -v args |grep -v COMMAND |awk '{print $7}'`
 docker exec etcd etcdctl --ca-file=/etcd-cert/ca.pem \
 --cert-file=/etcd-cert/etcd.pem \
 --key-file=/etcd-cert/etcd-key.pem \
