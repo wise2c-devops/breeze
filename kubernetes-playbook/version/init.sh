f1154125dba55cf879c43447ff0eb074d5c03b02
Alan Peng <peng.alan@gmail.com>
2019-08-18 19:38:44 +0800

    Add calico

--- kubernetes-playbook/version/init.sh
+++ kubernetes-playbook/version/init.sh
@@ -52,20 +52,20 @@ calico_version=v`cat ${path}/components-version.txt |grep "Calico" |awk '{print 
 echo "=== downloading calico release package ==="
 curl -L -o ${path}/file/calico-${calico_version}.tgz https://github.com/projectcalico/calico/releases/download/${calico_version}/release-${calico_version}.tgz
 echo "=== calico release package is downloaded successfully ==="
-
 tar zxf ${path}/file/calico-${calico_version}.tgz -C ${path}/file/
 mv ${path}/file/release-${calico_version} ${path}/file/calico
 rm -f ${path}/file/calico/bin/calicoctl-darwin-amd64
 rm -f ${path}/file/calico/bin/calicoctl-windows-amd64.exe
 docker pull calico/pod2daemon-flexvol:${calico_version}
 docker save calico/pod2daemon-flexvol:${calico_version} -o ${path}/file/calico/images/calico-pod2daemon-flexvol.tar
-echo "=== bzip calico images ==="
+echo "=== Compressing calico images ==="
 bzip2 -z --best ${path}/file/calico/images/calico-cni.tar
 bzip2 -z --best ${path}/file/calico/images/calico-kube-controllers.tar
 bzip2 -z --best ${path}/file/calico/images/calico-node.tar
 bzip2 -z --best ${path}/file/calico/images/calico-pod2daemon-flexvol.tar
 bzip2 -z --best ${path}/file/calico/images/calico-typha.tar
-echo "=== calico images are bzipped successfully ==="
+echo "=== Calico images are compressed as bzip format successfully ==="
+echo "calico_version: ${calico_version}" >> ${path}/yat/all.yml.gotmpl
 
 dashboard_repo=${kubernetes_repo}
 dashboard_version=v`cat ${path}/components-version.txt |grep "Dashboard" |awk '{print $3}'`
