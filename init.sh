8018b15353e95d88dacab13c775f982255743295
Alan Peng <peng.alan@gmail.com>
2019-10-28 11:29:13 +0800

    Upgrade Calico to v3.10.0

--- init.sh
+++ init.sh
@@ -16,7 +16,7 @@ kube_prometheus_version=0.2.0
 metrics_server_version=0.3.6
 dashboard_version=1.10.1
 flannel_version=0.11.0
-calico_version=3.9.2
+calico_version=3.10.0
 helm_version=2.15.1
 istio_version=1.3.3
 
