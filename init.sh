bd27bfe603ef4988e41e8e0d9fc78e65955c99a1
Alan Peng <peng.alan@gmail.com>
2019-11-05 19:10:03 +0800

    Update Istio and Harbor

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ set -e
 path=`dirname $0`
 
 kubernetes_version=1.13.12
-harbor_version=1.9.1
+harbor_version=1.9.2
 docker_version=18.06.3
 haproxy_version=2.0.0
 keepalived_version=1.3.5
@@ -18,7 +18,7 @@ dashboard_version=1.10.1
 flannel_version=0.11.0
 calico_version=3.9.2
 helm_version=2.14.3
-istio_version=1.3.3
+istio_version=1.3.4
 
 mv ${path}/kubernetes-playbook/version ${path}/kubernetes-playbook/v${kubernetes_version}
 mv ${path}/harbor-playbook/version ${path}/harbor-playbook/v${harbor_version}
