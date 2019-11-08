b777f20ba1c8431d824f45f59dd6a1ab9a0fb401
Alan Peng <peng.alan@gmail.com>
2019-11-08 19:22:05 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -17,8 +17,8 @@ metrics_server_version=0.3.6
 dashboard_version=1.10.1
 flannel_version=0.11.0
 calico_version=3.10.1
-helm_version=2.16.0
-istio_version=1.3.4
+helm_version=2.15.1
+istio_version=1.3.3
 
 mv ${path}/kubernetes-playbook/version ${path}/kubernetes-playbook/v${kubernetes_version}
 mv ${path}/harbor-playbook/version ${path}/harbor-playbook/v${harbor_version}
