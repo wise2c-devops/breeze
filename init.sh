b045ea625dd3cf61150b136eb68785390b2e0a14
Alan Peng <peng.alan@gmail.com>
2019-11-12 12:23:31 +0800

    Update Istio to v1.3.5

--- init.sh
+++ init.sh
@@ -18,7 +18,7 @@ dashboard_version=1.10.1
 flannel_version=0.11.0
 calico_version=3.9.3
 helm_version=2.14.3
-istio_version=1.3.4
+istio_version=1.3.5
 
 mv ${path}/kubernetes-playbook/version ${path}/kubernetes-playbook/v${kubernetes_version}
 mv ${path}/harbor-playbook/version ${path}/harbor-playbook/v${harbor_version}
