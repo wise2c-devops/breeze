40f5e55a7f05cf3a31ce9beeeb0281e5a7ea46f6
Alan Peng <peng.alan@gmail.com>
2019-11-25 16:29:40 +0800

    Add ECK (Elastic Cloud on K8s)

--- elasticcloud-playbook/version/init.sh
+++ elasticcloud-playbook/version/init.sh
@@ -7,13 +7,6 @@ path=`dirname $0`
 ElasticCloudVersion=`cat ${path}/components-version.txt |grep "ElasticCloud" |awk '{print $3}'`
 ElasticStackVersion=`cat ${path}/components-version.txt |grep "ElasticStack" |awk '{print $3}'`
 
-echo "Debug info 1:"
-cat ${path}/components-version.txt
-echo "Debug info 2:"
-echo $ElasticCloudVersion
-echo "Debug info 3:"
-echo $ElasticStackVersion
-
 echo "" >> ${path}/group_vars/elasticcloud.yml
 echo "elastic_cloud_version: ${ElasticCloudVersion}" >> ${path}/group_vars/elasticcloud.yml
 echo "elastic_stack_version: ${ElasticStackVersion}" >> ${path}/group_vars/elasticcloud.yml
@@ -21,8 +14,8 @@ echo "elastic_stack_version: ${ElasticStackVersion}" >> ${path}/group_vars/elast
 curl -L -o ${path}/file/eck.yaml https://download.elastic.co/downloads/eck/${ElasticCloudVersion}/all-in-one.yaml
 
 cat ${path}/file/eck.yaml |grep "image: docker.elastic.co/eck/" |awk -F":" '{print $2":"$3}' > images-list.txt
-echo 'docker.elastic.co/elasticsearch/elasticsearch:${ElasticStackVersion}' >> images-list.txt
-echo 'docker.elastic.co/kibana/kibana:${ElasticStackVersion}' >> images-list.txt
+echo "docker.elastic.co/elasticsearch/elasticsearch:${ElasticStackVersion}" >> images-list.txt
+echo "docker.elastic.co/kibana/kibana:${ElasticStackVersion}" >> images-list.txt
 
 echo 'Images list for Elastic Cloud:'
 cat images-list.txt
