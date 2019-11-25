7ec99039165a63cb9fc991d7dc1c6097edfb9eda
Alan Peng <peng.alan@gmail.com>
2019-11-25 16:11:27 +0800

    Add ECK (ElasticCloud on K8s)

---
+++ elasticcloud-playbook/version/init.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+set -e
+
+path=`dirname $0`
+
+ElasticCloudVersion=`cat ${path}/components-version.txt |grep "ElasticCloud" |awk '{print $3}'`
+ElasticStackVersion=`cat ${path}/components-version.txt |grep "ElasticCloud" |awk '{print $3}'`
+
+echo "" >> ${path}/group_vars/elasticcloud.yml
+echo "istio_version: ${IstioVersion}" >> ${path}/group_vars/elasticcloud.yml
+
+curl -L -o ${path}/file/eck.yaml https://download.elastic.co/downloads/eck/${ElasticCloudVersion}/all-in-one.yaml
+
+cat ${path}/file/eck.yaml |grep "image: docker.elastic.co/eck/" |awk -F":" '{print $2":"$3}' > images-list.txt
+echo 'docker.elastic.co/elasticsearch/elasticsearch:${ElasticStackVersion}' >> images-list.txt
+echo 'docker.elastic.co/kibana/kibana:${ElasticStackVersion}' >> images-list.txt
+
+echo 'Images list for Elastic Cloud:'
+cat images-list.txt
+
+for file in $(cat images-list.txt); do docker pull $file; done
+echo 'Images pulled.'
+
+docker save $(cat images-list.txt) -o elastic-cloud-images.tar
+echo 'Images saved.'
+bzip2 -z --best elastic-cloud-images.tar
+echo 'Images are compressed as bzip format.'
