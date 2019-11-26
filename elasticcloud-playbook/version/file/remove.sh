c12707ec28035ba6d480bd03ae3a58a9955bb0a9
Alan Peng <peng.alan@gmail.com>
2019-11-25 21:27:14 -0500

    Add ECK (ElasticCloud on K8s)

--- elasticcloud-playbook/version/file/remove.sh
+++ elasticcloud-playbook/version/file/remove.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 kubectl delete -f /var/tmp/wise2c/elasticcloud/kibana-service.yml
 kubectl delete -f /var/tmp/wise2c/elasticcloud/elasticsearch-service.yml
+kubectl delete kibana quickstart
+kubectl delete elasticsearch quickstart
 kubectl delete -f /var/tmp/wise2c/elasticcloud/kibana.yml
 kubectl delete -f /var/tmp/wise2c/elasticcloud/elasticsearch.yml
 kubectl delete -f /var/tmp/wise2c/elasticcloud/eck.yml
