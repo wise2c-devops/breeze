76980ff0c2fafb5f58e3237c442618eb612cd780
Alan Peng <peng.alan@gmail.com>
2019-12-10 19:10:36 +0800

    Update init.sh

--- elasticcloud-playbook/version/init.sh
+++ elasticcloud-playbook/version/init.sh
@@ -31,6 +31,6 @@ echo 'Images are compressed as bzip format.'
 
 sed -i "s,docker.elastic.co/eck,{{ registry_endpoint }}/{{ registry_project }},g" ${path}/template/eck.yml.j2
 
-curl -L -o ${path}/template/fluentd.yml.j2 https://github.com/fluent/fluentd-kubernetes-daemonset/blob/master/fluentd-daemonset-elasticsearch-rbac.yaml
+curl -L -o ${path}/template/fluentd.yml.j2 https://raw.githubusercontent.com/fluent/fluentd-kubernetes-daemonset/master/fluentd-daemonset-elasticsearch-rbac.yaml
 sed -i "s,fluent/fluentd-kubernetes-daemonset,{{ registry_endpoint }}/{{ registry_project }},g" ${path}/template/fluentd.yml.j2
 sed -i "s,elasticsearch-logging,quickstart-es-http.default.svc.cluster.local,g" ${path}/template/fluentd.yml.j2
