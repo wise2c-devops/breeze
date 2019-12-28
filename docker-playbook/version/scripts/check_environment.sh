f246bff6f8e7ddbe6823dae8ad412138ac01ae8a
Alan Peng <peng.alan@gmail.com>
2019-12-28 21:50:25 +0800

    Update check_environment.sh

--- docker-playbook/version/scripts/check_environment.sh
+++ docker-playbook/version/scripts/check_environment.sh
@@ -15,7 +15,7 @@ if [ "${BREEZE_LSB_ID}" != "RedHat" ] && [ "${BREEZE_LSB_ID}" != "CentOS" ] && [
 fi
 
 if [ "${BREEZE_LSB_ID}" == "RedHat" ] && [ `version_gt 7.3 ${BREEZE_LSB_RELEASE}` ]; then
-  echo "please use CentOS 7.4/7.5/7.6/7.7 for Breeze"
+  echo "please use RHEL 7.4/7.5/7.6/7.7 for Breeze"
   exit
 fi
 
