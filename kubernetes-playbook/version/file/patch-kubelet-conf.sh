f2d3ed0ef5fa779000e1eda5be4faf47e31b0853
Alan Peng <peng.alan@gmail.com>
2020-01-10 15:02:38 +0800

    patch kubelet.conf on master nodes

--- kubernetes-playbook/version/file/patch-kubelet-conf.sh
+++ kubernetes-playbook/version/file/patch-kubelet-conf.sh
@@ -4,3 +4,4 @@ cd /etc/kubernetes/
 cp -p /etc/kubernetes/kubelet.conf /etc/kubernetes/kubelet.conf.$TIME_STRING
 sed -i 's#client-certificate-data:.*$#client-certificate: /var/lib/kubelet/pki/kubelet-client-current.pem#g' kubelet.conf 
 sed -i 's#client-key-data:.*$#client-key: /var/lib/kubelet/pki/kubelet-client-current.pem#g' kubelet.conf
+systemctl restart kubelet
