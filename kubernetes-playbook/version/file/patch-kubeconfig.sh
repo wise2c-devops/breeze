af4329edb71c414aa900fbe3d4aba0dfbb2f900b
andyyoung01 <andyyoung01@gmail.com>
2020-01-23 17:34:57 +0800

    Patch kubeconfig file to use long term certificate (#101)
    Patch kubeconfig file to use long term certificate

---
+++ kubernetes-playbook/version/file/patch-kubeconfig.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /etc/kubernetes/
+
+kubectl config set-credentials kubernetes-admin --client-certificate=/etc/kubernetes/pki/admin.pem  --client-key=/etc/kubernetes/pki/admin-key.pem  --embed-certs=true --kubeconfig=admin.conf
+
+kubectl config set-credentials system:kube-controller-manager --client-certificate=/etc/kubernetes/pki/controller-manager.pem  --client-key=/etc/kubernetes/pki/controller-manager-key.pem  --embed-certs=true --kubeconfig=controller-manager.conf
+
+kubectl config set-credentials system:kube-scheduler --client-certificate=/etc/kubernetes/pki/scheduler.pem  --client-key=/etc/kubernetes/pki/scheduler-key.pem  --embed-certs=true --kubeconfig=scheduler.conf
+
+#restart controller-manager and scheduler
+docker ps|grep kube-controller-manager|awk '{print $1}'|xargs docker stop
+docker ps|grep kube-scheduler|awk '{print $1}'|xargs docker stop
