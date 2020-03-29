baa3ab1c0f6ab14f3801695122bd8f7594b6b503
Alan Peng <peng.alan@gmail.com>
2020-03-30 00:22:06 +0800

    Update etcd healthcheck script

--- etcd-playbook/version-by-kubeadm/file/make-etcd-snapshot.sh
+++ etcd-playbook/version-by-kubeadm/file/make-etcd-snapshot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-etcdurl=`docker exec etcd ps -o args |grep -v args |grep -v COMMAND |awk '{print $7}'`
-health=`docker exec etcd etcdctl --ca-file=/etcd-cert/ca.pem --cert-file=/etcd-cert/etcd.pem --key-file=/etcd-cert/etcd-key.pem --endpoints ${etcdurl} cluster-health |grep 'cluster is healthy'`
-if [ "$health" = "cluster is healthy" ]; then
+etcdurl=`docker inspect etcd |grep -A1 "\-\-advertise-client-urls" |grep https |head -n 1 |awk -F'"' '{print $2}'`
+docker exec etcd etcdctl --cacert=/etcd-cert/ca.pem --cert=/etcd-cert/etcd.pem --key=/etcd-cert/etcd-key.pem --endpoints ${etcdurl} endpoint health
+if [ $? -eq 0 ]; then
   echo cluster is healthy
   backuptime=`date +%F-%H-%M-%S`
-  etcdurl=`docker exec etcd ps -o args |grep -v COMMAND |awk '{print $7}'`
+  etcdurl=`docker inspect etcd |grep -A1 "\-\-advertise-client-urls" |grep https |head -n 1 |awk -F'"' '{print $2}'`
   etcdbackup=`docker exec -e ETCDCTL_API=3 etcd etcdctl --cacert=/etcd-cert/ca.pem --cert=/etcd-cert/etcd.pem --key=/etcd-cert/etcd-key.pem --endpoints ${etcdurl} snapshot save snapshotdb-${backuptime} |awk '{print $4}'`
   docker cp etcd:/${etcdbackup} ./
   docker exec -it etcd rm -f /${etcdbackup}
