60e720be7c67afbd8df88a70193321d96e1c1986
Alan Peng <peng.alan@gmail.com>
2020-03-30 00:22:59 +0800

    Update etcd healthcheck script

--- etcd-playbook/version-by-kubeadm/file/etcd-healthcheck.sh
+++ etcd-playbook/version-by-kubeadm/file/etcd-healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 etcdurl=`docker inspect etcd |grep -A1 "\-\-advertise-client-urls" |grep https |head -n 1 |awk -F'"' '{print $2}'`
-docker exec etcd etcdctl --ca-file=/etcd-cert/ca.pem \
---cert-file=/etcd-cert/etcd.pem \
---key-file=/etcd-cert/etcd-key.pem \
- --endpoints ${etcdurl} cluster-health |grep 'cluster is healthy'
+docker exec etcd etcdctl --cacert=/etcd-cert/ca.pem \
+--cert=/etcd-cert/etcd.pem \
+--key=/etcd-cert/etcd-key.pem \
+--endpoints ${etcdurl} endpoint health |grep 'is healthy'
