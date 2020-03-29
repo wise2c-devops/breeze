87c8afe0829fa9db4f46a09eaeaaef764474e7f8
Alan Peng <peng.alan@gmail.com>
2020-03-30 01:15:25 +0800

    change breeze temp folder

--- prometheus-playbook/version/file/add-on.sh
+++ prometheus-playbook/version/file/add-on.sh
@@ -9,9 +9,9 @@ etcd1_address=`cat etcd-address.txt |awk -F "," '{print $1}' |awk -F "//" '{prin
 etcd2_address=`cat etcd-address.txt |awk -F "," '{print $2}' |awk -F "//" '{print $2}' |awk -F ":" '{print $1}'`
 etcd3_address=`cat etcd-address.txt |awk -F "," '{print $3}' |awk -F "//" '{print $2}' |awk -F ":" '{print $1}'`
 
-sed -i "s/etcd_1_address/${etcd1_address}/g" /var/tmp/wise2c/prometheus/etcd.yaml
-sed -i "s/etcd_2_address/${etcd2_address}/g" /var/tmp/wise2c/prometheus/etcd.yaml
-sed -i "s/etcd_3_address/${etcd3_address}/g" /var/tmp/wise2c/prometheus/etcd.yaml
+sed -i "s/etcd_1_address/${etcd1_address}/g" /var/lib/wise2c/tmp/prometheus/etcd.yaml
+sed -i "s/etcd_2_address/${etcd2_address}/g" /var/lib/wise2c/tmp/prometheus/etcd.yaml
+sed -i "s/etcd_3_address/${etcd3_address}/g" /var/lib/wise2c/tmp/prometheus/etcd.yaml
 
 kubectl -n monitoring create secret generic etcd-certs --from-file=/etc/etcd/pki/ca.pem --from-file=/etc/etcd/pki/etcd.pem --from-file=/etc/etcd/pki/etcd-key.pem
 
