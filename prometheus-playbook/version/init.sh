2332d4e3b626f4e135a38cbad2f5b451b903dc83
Alan Peng <peng.alan@gmail.com>
2020-04-19 23:14:06 +0800

    Update Kube-Prometheus

--- prometheus-playbook/version/init.sh
+++ prometheus-playbook/version/init.sh
@@ -21,11 +21,11 @@ for file in $(grep -lr "grafana/grafana" kube-prometheus-$KubePrometheusVersion/
 for file in $(grep -lr "quay.io/prometheus" kube-prometheus-$KubePrometheusVersion/manifests/); do cat $file |grep "quay.io/prometheus" ; done >> image-lists-temp.txt
 for file in $(grep -lr "gcr.io/" kube-prometheus-$KubePrometheusVersion/manifests/); do cat $file |grep "gcr.io/" ; done >> image-lists-temp.txt
 
-prometheus_base_image=`cat kube-prometheus-$KubePrometheusVersion/manifests/prometheus-prometheus.yaml |grep "baseImage: " |awk '{print $2}'`
-prometheus_image_tag=`cat kube-prometheus-$KubePrometheusVersion/manifests/prometheus-prometheus.yaml |grep "version: " |awk '{print $2}'`
+prometheus_base_image=`cat kube-prometheus-$KubePrometheusVersion/manifests/prometheus-prometheus.yaml |grep "image: " |awk -F':' '{print $2}'`
+prometheus_image_tag=`cat kube-prometheus-$KubePrometheusVersion/manifests/prometheus-prometheus.yaml |grep "image " |awk -F':' '{print $3}'`
 
-alertmanager_base_image=`cat kube-prometheus-$KubePrometheusVersion/manifests/alertmanager-alertmanager.yaml |grep "baseImage: " |awk '{print $2}'`
-alertmanager_image_tag=`cat kube-prometheus-$KubePrometheusVersion/manifests/alertmanager-alertmanager.yaml |grep "version: " |awk '{print $2}'`
+alertmanager_base_image=`cat kube-prometheus-$KubePrometheusVersion/manifests/alertmanager-alertmanager.yaml |grep "image: " |awk -F':' '{print $2}'`
+alertmanager_image_tag=`cat kube-prometheus-$KubePrometheusVersion/manifests/alertmanager-alertmanager.yaml |grep "image: " |awk -F':' '{print $3}'`
 
 echo $prometheus_base_image:$prometheus_image_tag >> image-lists-temp.txt
 echo $alertmanager_base_image:$alertmanager_image_tag >> image-lists-temp.txt
@@ -35,13 +35,8 @@ rm -rf kube-prometheus-$KubePrometheusVersion
 sed "s/- --config-reloader-image=//g" image-lists-temp.txt > 1.txt
 sed "s/- --prometheus-config-reloader=//g" 1.txt > 2.txt
 sed "s/image: //g" 2.txt > 3.txt
-sed "s/repository: //g" 3.txt > 4.txt
-sed "s/baseImage: //g" 4.txt > 5.txt
-sed "s/- grafana/grafana/g" 5.txt > 6.txt
-cat 6.txt |grep ":" > 7.txt
-sed -i "s/[[:space:]]//g" 7.txt
-rm -f image-lists-temp.txt 1.txt 2.txt 3.txt 4.txt 5.txt 6.txt
-mv 7.txt images-list.txt
+rm -f image-lists-temp.txt 1.txt 2.txt
+mv 3.txt images-list.txt
 
 for file in $(cat images-list.txt); do docker pull $file; done
 echo 'Images pulled.'
