aea9d6416b1ec5bb2004344e863ce2e0e029999e
Alan Peng <peng.alan@gmail.com>
2020-06-13 23:48:07 +0800

    Update Istio

--- istio-playbook/version/file/deploy.sh
+++ istio-playbook/version/file/deploy.sh
@@ -17,44 +17,22 @@ for file in $(cat images-list.txt); do docker push $MyImageRepositoryIP/$MyImage
 
 echo 'Images pushed.'
 
-######### Update deploy yaml files #########
+# Istio deploy
 rm -rf istio-$IstioVersion
 tar zxvf istio-$IstioVersion-origin.tar.gz
 cd istio-$IstioVersion/
-
-sed -i "s/quay.io\/kiali/$MyImageRepositoryIP\/$MyImageRepositoryProject/g" samples/addons/kiali.yaml
-sed -i "s/docker.io\/jaegertracing/$MyImageRepositoryIP\/$MyImageRepositoryProject/g" samples/addons/jaeger.yaml
-sed -i "s/openzipkin\/zipkin-slim/$MyImageRepositoryIP\/$MyImageRepositoryProject\/zipkin-slim/g" samples/addons/extras/zipkin.yaml
-sed -i "s/prom\/prometheus/$MyImageRepositoryIP\/$MyImageRepositoryProject\/prometheus/g" samples/addons/prometheus.yaml
-sed -i "s/jimmidyson\/configmap-reload/$MyImageRepositoryIP\/$MyImageRepositoryProject\/configmap-reload/g" samples/addons/prometheus.yaml
-sed -i "s/grafana\/grafana/$MyImageRepositoryIP\/$MyImageRepositoryProject\/grafana/g" samples/addons/grafana.yaml
-
-#sed -i "s/hub: docker.io\/istio/hub: $MyImageRepositoryIP\/$MyImageRepositoryProject/g" manifests/profiles/default.yaml
-#sed -i "s/hub: docker.io\/prom/hub: $MyImageRepositoryIP\/$MyImageRepositoryProject/g" manifests/profiles/default.yaml
-#sed -i "s/hub: docker.io\/jaegertracing/hub: $MyImageRepositoryIP\/$MyImageRepositoryProject/g" manifests/profiles/default.yaml
-#sed -i "s/hub: docker.io\/openzipkin/hub: $MyImageRepositoryIP\/$MyImageRepositoryProject/g" manifests/profiles/default.yaml
-#sed -i "s/hub: docker.io\/omnition/hub: $MyImageRepositoryIP\/$MyImageRepositoryProject/g" manifests/profiles/default.yaml
-#sed -i "s/hub: quay.io\/kiali/hub: $MyImageRepositoryIP\/$MyImageRepositoryProject/g" manifests/profiles/default.yaml
-#sed -i "s/hub: quay.io\/kiali/hub: $MyImageRepositoryIP\/$MyImageRepositoryProject/g" manifests/charts/istio-telemetry/kiali/values.yaml
-#sed -i "s/hub: docker.io\/jaegertracing/hub: $MyImageRepositoryIP\/$MyImageRepositoryProject/g" manifests/charts/istio-telemetry/tracing/values.yaml 
-#sed -i "s/hub: docker.io\/openzipkin/hub: $MyImageRepositoryIP\/$MyImageRepositoryProject/g" manifests/charts/istio-telemetry/tracing/values.yaml
-#sed -i "s/hub: docker.io\/omnition/hub: $MyImageRepositoryIP\/$MyImageRepositoryProject/g" manifests/charts/istio-telemetry/tracing/values.yaml
-#sed -i "s/hub: docker.io\/prom/hub: $MyImageRepositoryIP\/$MyImageRepositoryProject/g" manifests/charts/istio-telemetry/prometheusOperator/values.yaml
-#sed -i "s/hub: docker.io\/prom/hub: $MyImageRepositoryIP\/$MyImageRepositoryProject/g" manifests/charts/istio-telemetry/prometheus/values.yaml
-
-# Istio init deploy
 rm -f /usr/bin/istioctl
 cp bin/istioctl /usr/bin/
-istioctl install --set profile=demo --set addonComponents.kiali.enabled=false --set addonComponents.prometheus.enabled=false --set addonComponents.grafana.enabled=false --set addonComponents.tracing.enabled=false --set hub=$MyImageRepositoryIP\/$MyImageRepositoryProject
 
-kubectl -n istio-system apply -f samples/addons/kiali.yaml
-kubectl -n istio-system apply -f samples/addons/jaeger.yaml
-kubectl -n istio-system apply -f samples/addons/prometheus.yaml
-kubectl -n istio-system apply -f samples/addons/grafana.yaml
+istioctl install --set profile=demo --set values.tracing.jaeger.hub=$MyImageRepositoryIP\/$MyImageRepositoryProject --set values.kiali.hub=$MyImageRepositoryIP\/$MyImageRepositoryProject --set values.prometheus.hub=$MyImageRepositoryIP\/$MyImageRepositoryProject --set values.grafana.image.repository=$MyImageRepositoryIP\/$MyImageRepositoryProject/grafana --set hub=$MyImageRepositoryIP\/$MyImageRepositoryProject
 
-kubectl -n istio-system apply -f /var/lib/wise2c/tmp/istio/kiali-service.yaml
-kubectl -n istio-system apply -f /var/lib/wise2c/tmp/istio/jaeger-service.yaml
-kubectl -n istio-system apply -f /var/lib/wise2c/tmp/istio/prometheus-service.yaml
-kubectl -n istio-system apply -f /var/lib/wise2c/tmp/istio/grafana-service.yaml
+#--set values.tracing.jaeger.tag=
+#--set values.prometheus.tag=
+#--set values.grafana.image.tag=
+
+kubectl apply -f /var/lib/wise2c/tmp/istio/kiali-service.yaml
+kubectl apply -f /var/lib/wise2c/tmp/istio/jaeger-service.yaml
+kubectl apply -f /var/lib/wise2c/tmp/istio/prometheus-service.yaml
+kubectl apply -f /var/lib/wise2c/tmp/istio/grafana-service.yaml
 
 echo 'NodePorts are set for services.'
--- istio-playbook/version/init.sh
+++ istio-playbook/version/init.sh
@@ -17,17 +17,16 @@ echo "istio/proxyv2:$IstioVersion" > images-list.txt
 echo "istio/pilot:$IstioVersion" >> images-list.txt
 echo "istio/mixer:$IstioVersion" >> images-list.txt
 echo "istio/operator:$IstioVersion" >> images-list.txt
-
+echo "prom/prometheus:"`cat istio-$IstioVersion/manifests/profiles/default.yaml |grep -A1 docker.io/prom |grep tag |awk '{print $2}'` >> images-list.txt
+echo "grafana/grafana:"`cat istio-$IstioVersion/manifests/profiles/default.yaml |grep -A1 grafana/grafana |grep tag |awk '{print $2}'` >> images-list.txt
+echo "jaegertracing/all-in-one:"`cat istio-$IstioVersion/manifests/profiles/default.yaml |grep -A1 docker.io/jaegertracing |grep tag |awk '{print $2}' |awk -F "[\"]" '{print $2}'` >> images-list.txt
+echo "quay.io/kiali/kiali:"`cat istio-$IstioVersion/manifests/profiles/default.yaml |grep -A 1 "hub: quay.io/kiali" |awk 'END{print}' |awk '{print $2}'` >> images-list.txt
+cat istio-$IstioVersion/samples/addons/kiali.yaml |grep image |awk -F ":" '{print $2":"$3}' |grep -v IfNotPresent >> images-list.txt
+cat istio-$IstioVersion/manifests/profiles/default.yaml |grep coredns-plugin |awk '{print $2}' >> images-list.txt
+#cat istio-$IstioVersion/samples/addons/prometheus.yaml |grep "image:" |awk -F':' '{print $2":"$3}' |awk -F "[\"]" '{print $2}' >> images-list.txt
+#echo "jaegertracing/zipkin-slim:"`cat istio-$IstioVersion/manifests/profiles/default.yaml |grep -A1 docker.io/openzipkin |grep tag |awk '{print $2}'` >> images-list.txt
 #echo omnition/opencensus-collector:`cat istio-$IstioVersion/manifests/charts/istio-telemetry/tracing/values.yaml |grep -A1 "hub: docker.io/omnition" |awk 'END{print}' |awk '{print $2}'` >> images-list.txt
 #echo omnition/opencensus-agent:`cat istio-$IstioVersion/manifests/charts/istio-telemetry/tracing/values.yaml |grep -A1 "hub: docker.io/omnition" |awk 'END{print}' |awk '{print $2}'` >> images-list.txt
-echo quay.io/kiali/kiali:`cat istio-$IstioVersion/manifests/profiles/default.yaml |grep -A 1 "hub: quay.io/kiali" |awk 'END{print}' |awk '{print $2}'` >> images-list.txt
-cat istio-$IstioVersion/samples/addons/kiali.yaml |grep image |awk -F ":" '{print $2":"$3}' |grep -v IfNotPresent >> images-list.txt
-cat istio-$IstioVersion/samples/addons/jaeger.yaml |grep image |awk -F "[\"]" '{print $2}' |awk -F "/" '{print $2"/"$3}' >> images-list.txt
-cat istio-$IstioVersion/samples/addons/extras/zipkin.yaml |grep image |awk -F':' '{print $2":"$3}' >> images-list.txt
-cat istio-$IstioVersion/samples/addons/prometheus.yaml |grep "image:" |awk -F':' '{print $2":"$3}' |awk -F "[\"]" '{print $2}' >> images-list.txt
-cat istio-$IstioVersion/samples/addons/grafana.yaml |grep image |awk -F "[\"]" '{print $2}' >> images-list.txt
-cat istio-$IstioVersion/manifests/profiles/default.yaml |grep coredns-plugin |awk '{print $2}' >> images-list.txt
-
 #echo "ubuntu:bionic" >> images-list.txt
 
 echo 'Images list for Istio:'
