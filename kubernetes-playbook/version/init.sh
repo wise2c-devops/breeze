820e39ab73bf05d314b666d4025b8d8b1465a114
Alan Peng <peng.alan@gmail.com>
2020-08-10 08:50:21 +0800

    Update metrics server to v0.3.7

--- kubernetes-playbook/version/init.sh
+++ kubernetes-playbook/version/init.sh
@@ -98,13 +98,13 @@ curl -sS https://raw.githubusercontent.com/kubernetes/dashboard/${dashboard_vers
 echo "=== pulling kubernetes dashboard and metrics-server images ==="
 docker pull ${dashboard_repo}/dashboard:${dashboard_version}
 docker pull ${dashboard_repo}/metrics-scraper:${metrics_scraper_version}
-docker pull ${metrics_server_repo}/metrics-server-amd64:${metrics_server_version}
+docker pull ${metrics_server_repo}/metrics-server/metrics-server:${metrics_server_version}
 echo "=== kubernetes dashboard and metrics-server images are pulled successfully ==="
 
 echo "=== saving kubernetes dashboard images ==="
 docker save ${dashboard_repo}/dashboard:${dashboard_version} -o ${path}/file/dashboard.tar
 docker save ${dashboard_repo}/metrics-scraper:${metrics_scraper_version} -o ${path}/file/metrics-scraper.tar
-docker save ${metrics_server_repo}/metrics-server-amd64:${metrics_server_version} -o ${path}/file/metrics-server.tar
+docker save ${metrics_server_repo}/metrics-server/metrics-server:${metrics_server_version} -o ${path}/file/metrics-server.tar
 rm -f ${path}/file/dashboard.tar.bz2
 rm -f ${path}/file/metrics-scraper.tar.bz2
 rm -f ${path}/file/metrics-server.tar.bz2
