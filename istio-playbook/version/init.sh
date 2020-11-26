30d2b8026bf8ec5f1dde8a606261db19adde6d07
Alan Peng <peng.alan@gmail.com>
2020-11-26 17:39:30 +0800

    Update init script for Istio new version

--- istio-playbook/version/init.sh
+++ istio-playbook/version/init.sh
@@ -15,19 +15,12 @@ cd ${path}/file/
 tar zxf istio-$IstioVersion-origin.tar.gz
 echo "istio/proxyv2:$IstioVersion" > images-list.txt
 echo "istio/pilot:$IstioVersion" >> images-list.txt
-#echo "istio/mixer:$IstioVersion" >> images-list.txt
 echo "istio/operator:$IstioVersion" >> images-list.txt
-echo "prom/prometheus:"`cat istio-$IstioVersion/manifests/profiles/default.yaml |grep -A1 docker.io/prom |grep tag |awk '{print $2}'` >> images-list.txt
-echo "grafana/grafana:"`cat istio-$IstioVersion/manifests/profiles/default.yaml |grep -A1 grafana/grafana |grep tag |awk '{print $2}'` >> images-list.txt
-echo "jaegertracing/all-in-one:"`cat istio-$IstioVersion/manifests/profiles/default.yaml |grep -A1 docker.io/jaegertracing |grep tag |awk '{print $2}' |awk -F "[\"]" '{print $2}'` >> images-list.txt
-echo "quay.io/kiali/kiali:"`cat istio-$IstioVersion/manifests/profiles/default.yaml |grep -A 1 "hub: quay.io/kiali" |awk 'END{print}' |awk '{print $2}'` >> images-list.txt
-#cat istio-$IstioVersion/samples/addons/kiali.yaml |grep image |awk -F ":" '{print $2":"$3}' |grep -v IfNotPresent >> images-list.txt
+cat istio-$IstioVersion/samples/addons/prometheus.yaml |grep image: |grep prom |awk '{print $2}' |awk -F "[\"]" '{print $2}' >> images-list.txt
+cat istio-$IstioVersion/samples/addons/grafana.yaml |grep image: |grep grafana |awk '{print $2}' |awk -F "[\"]" '{print $2}' >> images-list.txt
+cat istio-$IstioVersion/samples/addons/jaeger.yaml |grep image: |grep jaegertracing |awk '{print $2}' |awk -F "[\"]" '{print $2}' >> images-list.txt
+cat istio-$IstioVersion/samples/addons/kiali.yaml |grep image:  |awk '{print $3}' |awk -F "[\"]" '{print $2}' >> images-list.txt
 cat istio-$IstioVersion/manifests/profiles/default.yaml |grep coredns-plugin |awk '{print $2}' >> images-list.txt
-#cat istio-$IstioVersion/samples/addons/prometheus.yaml |grep "image:" |awk -F':' '{print $2":"$3}' |awk -F "[\"]" '{print $2}' >> images-list.txt
-#echo "jaegertracing/zipkin-slim:"`cat istio-$IstioVersion/manifests/profiles/default.yaml |grep -A1 docker.io/openzipkin |grep tag |awk '{print $2}'` >> images-list.txt
-#echo omnition/opencensus-collector:`cat istio-$IstioVersion/manifests/charts/istio-telemetry/tracing/values.yaml |grep -A1 "hub: docker.io/omnition" |awk 'END{print}' |awk '{print $2}'` >> images-list.txt
-#echo omnition/opencensus-agent:`cat istio-$IstioVersion/manifests/charts/istio-telemetry/tracing/values.yaml |grep -A1 "hub: docker.io/omnition" |awk 'END{print}' |awk '{print $2}'` >> images-list.txt
-#echo "ubuntu:bionic" >> images-list.txt
 
 echo 'Images list for Istio:'
 cat images-list.txt
