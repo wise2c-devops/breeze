6c2c383d4c3455a4cd277d1b89564f189f1acc28
Alan Peng <peng.alan@gmail.com>
2021-04-09 11:31:12 +0800

    remove Clair

--- harbor-playbook/version/file/install-harbor.sh
+++ harbor-playbook/version/file/install-harbor.sh
@@ -4,4 +4,4 @@ sed -i 's,port: 443,# port: 443,g' harbor.yml
 sed -i 's,certificate:,# certificate:,g' harbor.yml
 sed -i 's,private_key:,# private_key:,g' harbor.yml
 
-./install.sh --with-clair --with-chartmuseum
+./install.sh --with-chartmuseum
