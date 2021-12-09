9b32e9a211f98e438a21c290657f3595811ae8a5
Alan Peng <peng.alan@gmail.com>
2021-12-09 00:45:57 -0500

    Upgrade Kubernetes to v1.20.13

--- init.sh
+++ init.sh
@@ -4,23 +4,23 @@ set -e
 
 path=`dirname $0`
 
-kubernetes_version=1.20.12
+kubernetes_version=1.20.13
 harbor_version=2.4.0
 docker_version=20.10.10
-crio_version=1.20.5
+crio_version=1.20.6
 haproxy_version=2.0.0
 keepalived_version=1.3.5
 loadbalancer_version=HAProxy-${haproxy_version}_Keepalived-${keepalived_version}
 prometheus_version=2.26.0
 prometheus_operator_version=0.47.0
 kube_prometheus_version=0.8.0
-metrics_server_version=0.5.1
+metrics_server_version=0.5.2
 dashboard_version=2.4.0
 metrics_scraper_version=1.0.7
-flannel_version=0.15.0
-calico_version=3.20.2
-helm_version=3.7.1
-istio_version=1.11.4
+flannel_version=0.15.1
+calico_version=3.21.2
+helm_version=3.7.2
+istio_version=1.12.1
 contour_version=1.19.1
 contour_envoyproxy_version=1.19.1
 elastic_cloud_version=1.6.0
