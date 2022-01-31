1ab9386fc83222df31966773f1d5f602c9da09a3
Alan Peng <peng.alan@gmail.com>
2022-01-31 22:02:36 +0800

    Upgrade ECK version

--- elasticcloud-playbook/version/init.sh
+++ elasticcloud-playbook/version/init.sh
@@ -11,13 +11,14 @@ echo "" >> ${path}/group_vars/elasticcloud.yml
 echo "elastic_cloud_version: ${ElasticCloudVersion}" >> ${path}/group_vars/elasticcloud.yml
 echo "elastic_stack_version: ${ElasticStackVersion}" >> ${path}/group_vars/elasticcloud.yml
 
-curl -L -o ${path}/template/eck.yml.j2 https://download.elastic.co/downloads/eck/${ElasticCloudVersion}/all-in-one.yaml
+curl -L -o ${path}/template/crds.yml.j2 https://download.elastic.co/downloads/eck/${ElasticCloudVersion}/crds.yaml
+curl -L -o ${path}/template/eck.yml.j2 https://download.elastic.co/downloads/eck/${ElasticCloudVersion}/operator.yaml
 
 cat ${path}/template/eck.yml.j2 |grep 'image: "docker.elastic.co/eck/' |awk -F":" '{print $2":"$3}' |awk -F'"' '{print $2}' > images-list.txt
 echo "docker.elastic.co/elasticsearch/elasticsearch:${ElasticStackVersion}" >> images-list.txt
 echo "docker.elastic.co/kibana/kibana:${ElasticStackVersion}" >> images-list.txt
 echo "docker.elastic.co/beats/filebeat:${ElasticStackVersion}" >> images-list.txt
-echo "fluent/fluentd-kubernetes-daemonset:v1-debian-elasticsearch" >> images-list.txt
+#echo "fluent/fluentd-kubernetes-daemonset:v1-debian-elasticsearch" >> images-list.txt
 
 echo 'Images list for Elastic Cloud:'
 cat images-list.txt
