19470ddedce77b12b67cfdc4ee146b57561b6a88
Alan Peng <peng.alan@gmail.com>
2022-06-12 01:46:47 +0800

    Upgrade components version

--- docker-playbook/version/scripts/check_environment.sh
+++ docker-playbook/version/scripts/check_environment.sh
@@ -8,19 +8,15 @@ function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" 
 function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" == "$1"; }
 function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
 function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
-
 if version_gt $VERSION $VERSION2; then
    echo "$VERSION is greater than $VERSION2"
 fi
-
 if version_le $VERSION $VERSION2; then
    echo "$VERSION is less than or equal to $VERSION2"
 fi
-
 if version_lt $VERSION $VERSION2; then
    echo "$VERSION is less than $VERSION2"
 fi
-
 if version_ge $VERSION $VERSION2; then
    echo "$VERSION is greater than or equal to $VERSION2"
 fi
@@ -30,7 +26,7 @@ fi
 [ ${BREEZE_LSB_RELEASE} ]
 [ ${BREEZE_PYTHON_VERSION} ]
 
-if [[ "${BREEZE_LSB_ID}" != "RedHat" ]] && [[ "${BREEZE_LSB_ID}" != "CentOS" ]] && [[ "${BREEZE_LSB_ID}" != "Ubuntu" ]]; then
+if [[ "${BREEZE_LSB_ID}" != "RedHat" ]] && [[ "${BREEZE_LSB_ID}" != "CentOS" ]] && [[ "${BREEZE_LSB_ID}" != "OracleLinux" ]] && [[ "${BREEZE_LSB_ID}" != "Rocky" ]] && [[ "${BREEZE_LSB_ID}" != "AlmaLinux" ]] && [[ "${BREEZE_LSB_ID}" != "Anolis" ]] && [[ "${BREEZE_LSB_ID}" != "Ubuntu" ]]; then
   echo "please use RHEL or CentOS or Ubuntu"
   exit
 fi
@@ -45,8 +41,8 @@ if version_gt 7.4 ${BREEZE_LSB_RELEASE} && [[ "${BREEZE_LSB_ID}" == "CentOS" ]];
   exit
 fi
 
-if version_gt 16 ${BREEZE_LSB_RELEASE} && [[ "${BREEZE_LSB_ID}" == "Ubuntu" ]]; then
-  echo "please use Ubuntu 16/18 for Breeze"
+if version_gt 18 ${BREEZE_LSB_RELEASE} && [[ "${BREEZE_LSB_ID}" == "Ubuntu" ]]; then
+  echo "please use Ubuntu 18/20 for Breeze"
   exit
 fi
 
--- init.sh
+++ init.sh
@@ -5,9 +5,8 @@ set -e
 path=`dirname $0`
 
 kubernetes_version=1.23.7
-harbor_version=2.5.0
-docker_version=20.10.7
-crio_version=1.23.2
+harbor_version=2.5.1
+docker_version=20.10.17
 haproxy_version=2.0.0
 keepalived_version=1.3.5
 loadbalancer_version=HAProxy-${haproxy_version}_Keepalived-${keepalived_version}
@@ -16,12 +15,12 @@ prometheus_operator_version=0.49.0
 kube_prometheus_version=0.9.0
 metrics_server_version=0.6.1
 dashboard_version=2.5.1
-metrics_scraper_version=1.0.7
-flannel_version=0.18.0
+metrics_scraper_version=1.0.8
+flannel_version=0.18.1
 flannel_cni_plugin_version=1.1.0
 calico_version=3.23.1
 helm_version=3.9.0
-istio_version=1.13.4
+istio_version=1.14.1
 contour_version=1.21.0
 contour_envoyproxy_version=1.22.0
 elastic_cloud_version=2.2.0
@@ -30,7 +29,6 @@ elastic_stack_version=8.2.2
 mv ${path}/kubernetes-playbook/version ${path}/kubernetes-playbook/v${kubernetes_version}
 mv ${path}/harbor-playbook/version ${path}/harbor-playbook/v${harbor_version}
 mv ${path}/docker-playbook/version ${path}/docker-playbook/${docker_version}-CE
-mv ${path}/crio-playbook/version ${path}/crio-playbook/${crio_version}
 mv ${path}/loadbalancer-playbook/version ${path}/loadbalancer-playbook/${loadbalancer_version}
 mv ${path}/prometheus-playbook/version ${path}/prometheus-playbook/Kube-Prometheus-v${kube_prometheus_version}
 mv ${path}/istio-playbook/version ${path}/istio-playbook/v${istio_version}
@@ -45,7 +43,6 @@ echo "ETCD Version: ${etcd_version}" > ${path}/components-version.txt
 echo "Kubernetes Version: ${kubernetes_version}" >> ${path}/components-version.txt
 echo "Harbor Version: ${harbor_version}" >> ${path}/components-version.txt
 echo "Docker Version: ${docker_version}" >> ${path}/components-version.txt
-echo "CRIO Version: ${crio_version}" >> ${path}/components-version.txt
 echo "HAProxy Version: ${haproxy_version}" >> ${path}/components-version.txt
 echo "Keepalived Version: ${keepalived_version}" >> ${path}/components-version.txt
 echo "Prometheus Version: ${prometheus_version}" >> ${path}/components-version.txt
