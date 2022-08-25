e0c01b4695d2838315bb961dc15f1050bf735d48
Alan Peng <peng.alan@gmail.com>
2022-08-25 12:29:32 +0800

    Upgrade Kubernetes to v1.23.9

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ path=`dirname $0`
 
 kubernetes_version=1.23.9
 harbor_version=2.5.1
-docker_version=20.10.7
+docker_version=20.10.17
 crio_version=1.23.3
 haproxy_version=2.0.0
 keepalived_version=1.3.5
