688e9a8c6cc4a73bc892b529167d08c892ef074c
Alan Peng <peng.alan@gmail.com>
2022-08-27 16:02:30 +0800

    Upgrade Kubernetes to v1.23.9

--- init.sh
+++ init.sh
@@ -4,27 +4,27 @@ set -e
 
 path=`dirname $0`
 
-kubernetes_version=1.23.10
-harbor_version=2.5.1
+kubernetes_version=1.23.9
+harbor_version=2.5.3
 docker_version=20.10.17
 haproxy_version=2.0.0
 keepalived_version=1.3.5
 loadbalancer_version=HAProxy-${haproxy_version}_Keepalived-${keepalived_version}
-prometheus_version=2.29.1
-prometheus_operator_version=0.49.0
-kube_prometheus_version=0.9.0
+prometheus_version=2.36.1
+prometheus_operator_version=0.57.0
+kube_prometheus_version=0.11.0
 metrics_server_version=0.6.1
 dashboard_version=2.5.1
-metrics_scraper_version=1.0.7
-flannel_version=0.18.1
+metrics_scraper_version=1.0.8
+flannel_version=0.19.1
 flannel_cni_plugin_version=1.1.0
-calico_version=3.23.2
-helm_version=3.9.0
-istio_version=1.14.1
-contour_version=1.21.1
-contour_envoyproxy_version=1.22.2
-elastic_cloud_version=2.2.0
-elastic_stack_version=8.2.3
+calico_version=3.24.0
+helm_version=3.9.4
+istio_version=1.14.3
+contour_version=1.22.0
+contour_envoyproxy_version=1.23.0
+elastic_cloud_version=2.4.0
+elastic_stack_version=8.4.0
 
 mv ${path}/kubernetes-playbook/version ${path}/kubernetes-playbook/v${kubernetes_version}
 mv ${path}/harbor-playbook/version ${path}/harbor-playbook/v${harbor_version}
