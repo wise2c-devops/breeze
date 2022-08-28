9115f403068c3826a7000edb52d42f6c7d2cf029
Alan Peng <peng.alan@gmail.com>
2022-08-28 10:37:46 +0800

    Upgrade Kubernetes to v1.23.9

--- prometheus-playbook/version/file/deploy.sh
+++ prometheus-playbook/version/file/deploy.sh
@@ -53,7 +53,7 @@ kctl() {
     kubectl --namespace "$NAMESPACE" "$@"
 }
 
-kubectl apply ---server-side -f manifests/setup
+kubectl apply --server-side -f manifests/setup
 
 # Wait for CRDs to be ready.
 printf "Waiting for Operator to register custom resource definitions..."
