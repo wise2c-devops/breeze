416fafc30388042776ff5392899908e7347972d2
Alan Peng <peng.alan@gmail.com>
2022-09-26 14:42:32 +0800

    Upgrade Kubernetes to v1.22.13

--- init.sh
+++ init.sh
@@ -4,9 +4,9 @@ set -e
 
 path=`dirname $0`
 
-kubernetes_version=1.22.12
+kubernetes_version=1.22.13
 harbor_version=2.5.3
-docker_version=20.10.17
+docker_version=20.10.18
 crio_version=1.22.5
 haproxy_version=2.0.0
 keepalived_version=1.3.5
