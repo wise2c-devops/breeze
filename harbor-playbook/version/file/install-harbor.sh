1c8cbe0b65a265017f7da84bdf9533bac7b4608b
Alan Peng <peng.alan@gmail.com>
2023-10-18 03:13:52 -0400

    Update install script for new Harbor version

--- harbor-playbook/version/file/install-harbor.sh
+++ harbor-playbook/version/file/install-harbor.sh
@@ -9,3 +9,15 @@ sed -i 's,private_key:,# private_key:,g' harbor.yml
 #set -e
 
 ./install.sh
+
+# How to add Harbor as Helm Charts repo?
+# helm repo add --username={{ registry_user }} --password={{ registry_password }} {{ registry_project }} http://{{ registry_endpoint }}/chartrepo/{{ registry_project }}
+
+# How to login Harbor Helm Charts repo?
+#helm registry login http://{{ registry_endpoint }} --insecure--username {{ registry_user }} --password {{ registry_password }}
+
+# How to push charts package to Harbor?
+#helm push --insecure-skip-tls-verify hello-0.1.0.tgz oci://{{ registry_endpoint }}/library
+
+# How to pull charts package from Harbor?
+#helm pull --insecure-skip-tls-verify oci://{{ registry_endpoint }}/library/hello
