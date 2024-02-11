584fb1bfb11ddc5d9921c848d618875aca79881b
Alan Peng <peng.alan@gmail.com>
2024-02-11 20:07:52 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,10 +4,10 @@ set -e
 
 path=`dirname $0`
 
-kubernetes_version=1.28.2
-harbor_version=2.9.0
-docker_version=23.0.1
-crio_version=1.28.1
+kubernetes_version=1.28.6
+harbor_version=2.10.0
+docker_version=24.0.6
+crio_version=1.28.3
 haproxy_version=2.0.0
 keepalived_version=1.3.5
 loadbalancer_version=HAProxy-${haproxy_version}_Keepalived-${keepalived_version}
@@ -17,15 +17,15 @@ kube_prometheus_version=0.13.0
 metrics_server_version=0.6.4
 dashboard_version=2.7.0
 metrics_scraper_version=1.0.8
-flannel_version=0.22.3
-flannel_cni_plugin_version=1.2.0
-calico_version=3.26.1
-helm_version=3.12.3
-istio_version=1.19.0
-contour_version=1.26.0
-contour_envoyproxy_version=1.27.0
-elastic_cloud_version=2.9.0
-elastic_stack_version=8.10.1
+flannel_version=0.24.2
+flannel_cni_plugin_version=v1.4.0-flannel1
+calico_version=3.27.0
+helm_version=3.14.0
+istio_version=1.20.3
+contour_version=1.27.0
+contour_envoyproxy_version=1.28.0
+elastic_cloud_version=2.11.1
+elastic_stack_version=8.12.1
 
 mv ${path}/kubernetes-playbook/version ${path}/kubernetes-playbook/v${kubernetes_version}
 mv ${path}/harbor-playbook/version ${path}/harbor-playbook/v${harbor_version}
