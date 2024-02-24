adac85434f3630682387bed4d873e20ec07e39e4
Alan Peng <peng.alan@gmail.com>
2024-02-24 02:51:12 -0500

    Update etcd-playbook

--- etcd-playbook/version-by-kubeadm/file/etcd-healthcheck.sh
+++ etcd-playbook/version-by-kubeadm/file/etcd-healthcheck.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-etcdurl=`docker inspect etcd |grep -A1 "\-\-advertise-client-urls" |grep https |head -n 1 |awk -F'"' '{print $2}'`
-docker exec etcd etcdctl --cacert=/etcd-cert/ca.pem \
+etcdurl=`podman inspect etcd |grep -A1 "\-\-advertise-client-urls" |grep https |head -n 1 |awk -F'"' '{print $2}' |awk -F'=' '{print $2}'`
+podman exec etcd etcdctl --cacert=/etcd-cert/ca.pem \
 --cert=/etcd-cert/etcd.pem \
 --key=/etcd-cert/etcd-key.pem \
 --endpoints ${etcdurl} endpoint health |grep 'is healthy'
-docker exec etcd etcdctl --cacert=/etcd-cert/ca.pem \
+podman exec etcd etcdctl --cacert=/etcd-cert/ca.pem \
 --cert=/etcd-cert/etcd.pem \
 --key=/etcd-cert/etcd-key.pem \
 --endpoints ${etcdurl} endpoint status --cluster -w table
