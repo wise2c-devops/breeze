b8514ad6c41ce6e19c83b8ea06f3e95806a23bd8
Alan Peng <peng.alan@gmail.com>
2024-02-24 03:37:16 -0500

    Update kubernetes-playbook

--- harbor-playbook/version/file/install-harbor.sh
+++ harbor-playbook/version/file/install-harbor.sh
@@ -4,4 +4,12 @@ sed -i 's,port: 443,# port: 443,g' harbor.yml
 sed -i 's,certificate:,# certificate:,g' harbor.yml
 sed -i 's,private_key:,# private_key:,g' harbor.yml
 
-./install.sh --with-chartmuseum
+./install.sh
+
+#fix the bug for ARM64 packages
+export CPUArch=$(uname -m | awk '{ if ($1 == "x86_64") print "amd64"; else if ($1 == "aarch64") print "arm64"; else print $1 }')
+
+if [ $CPUArch == 'aarch64' ]
+then
+  chmod -R 777 /data/redis
+fi
--- harbor-playbook/version/init.sh
+++ harbor-playbook/version/init.sh
@@ -9,10 +9,18 @@ version=`cat ${path}/components-version.txt |grep "Harbor" |awk '{print $3}'`
 echo "" >> ${path}/yat/harbor.yml.gotmpl
 echo "version: v${version}" >> ${path}/yat/harbor.yml.gotmpl
 
-curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m) -o ${path}/file/docker-compose
+curl -L https://github.com/docker/compose/releases/download/2.24.6/docker-compose-$(uname -s)-$(uname -m) -o ${path}/file/docker-compose
 
-curl -L https://github.com/wise2c-devops/build-harbor-aarch64/releases/download/v${version}/harbor-offline-installer-aarch64-v${version}.tgz \
+export CPUArch=$(uname -m | awk '{ if ($1 == "x86_64") print "amd64"; else if ($1 == "aarch64") print "arm64"; else print $1 }')
+
+if [ $CPUArch == 'amd64' ]
+then
+   curl -L https://storage.googleapis.com/harbor-releases/release-${version%.*}.0/harbor-offline-installer-v${version}.tgz \
     -o ${path}/file/harbor-offline-installer-v${version}.tgz
+else
+  curl -L https://github.com/wise2c-devops/build-harbor-aarch64/releases/download/v${version}/harbor-offline-installer-aarch64-v${version}.tgz \
+    -o ${path}/file/harbor-offline-installer-v${version}.tgz
+fi
 
 curl -sSL https://raw.githubusercontent.com/vmware/harbor/v${version}/make/harbor.yml.tmpl \
     | sed \
