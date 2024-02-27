cb5d4209ebd0de9e8d656f135ea0bf0b953e7e1a
Alan Peng <peng.alan@gmail.com>
2024-02-27 05:15:04 -0500

    change command name docker to podman

--- prometheus-playbook/version/file/deploy.sh
+++ prometheus-playbook/version/file/deploy.sh
@@ -13,11 +13,11 @@ PrometheusOperatorVersion=`cat components-version.txt |grep "PrometheusOperator 
 NAMESPACE=monitoring
 
 ######### Push images #########
-for file in $(cat images-list.txt); do docker tag $file $MyImageRepositoryIP/$MyImageRepositoryProject/${file##*/}; done
+for file in $(cat images-list.txt); do podman tag $file $MyImageRepositoryIP/$MyImageRepositoryProject/${file##*/}; done
 
 echo 'Images taged.'
 
-for file in $(cat images-list.txt); do docker push $MyImageRepositoryIP/$MyImageRepositoryProject/${file##*/}; done
+for file in $(cat images-list.txt); do podman push $MyImageRepositoryIP/$MyImageRepositoryProject/${file##*/}; done
 
 echo 'Images pushed.'
 ######### Update deploy yaml files #########
