e9e95c38e477e6625ba5aeb9a73d48b916ff1bfe
Alan Peng <peng.alan@gmail.com>
2024-04-05 09:51:49 -0400

    Update components

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ set -e
 
 path=`dirname $0`
 
-kubernetes_version=1.29.2
-harbor_version=2.10.0
+kubernetes_version=1.29.3
+harbor_version=2.10.1
 docker_version=25.0.3
 crio_version=1.29.2
 haproxy_version=2.0.0
@@ -14,18 +14,18 @@ loadbalancer_version=HAProxy-${haproxy_version}_Keepalived-${keepalived_version}
 prometheus_version=2.46.0
 prometheus_operator_version=0.67.1
 kube_prometheus_version=0.13.0
-metrics_server_version=0.6.4
+metrics_server_version=0.7.1
 dashboard_version=2.7.0
 metrics_scraper_version=1.0.8
-flannel_version=0.24.2
+flannel_version=0.24.4
 flannel_cni_plugin_version=1.4.0-flannel1
-calico_version=3.27.2
-helm_version=3.14.2
-istio_version=1.20.3
-contour_version=1.28.1
-contour_envoyproxy_version=1.29.1
-elastic_cloud_version=2.11.1
-elastic_stack_version=8.12.2
+calico_version=3.27.3
+helm_version=3.14.3
+istio_version=1.21.0
+contour_version=1.28.2
+contour_envoyproxy_version=1.29.2
+elastic_cloud_version=2.12.1
+elastic_stack_version=8.13.1
 
 mv ${path}/kubernetes-playbook/version ${path}/kubernetes-playbook/v${kubernetes_version}
 mv ${path}/harbor-playbook/version ${path}/harbor-playbook/v${harbor_version}
