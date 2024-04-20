6c309c83de929684155188c7341f33b667b9b878
Alan Peng <peng.alan@gmail.com>
2024-04-19 22:55:59 -0400

    Upgrade Kubernetes to v1.29.4

--- init.sh
+++ init.sh
@@ -4,10 +4,10 @@ set -e
 
 path=`dirname $0`
 
-kubernetes_version=1.29.3
-harbor_version=2.10.1
+kubernetes_version=1.29.4
+harbor_version=2.10.2
 docker_version=25.0.3
-crio_version=1.29.2
+crio_version=1.29.4
 haproxy_version=2.0.0
 keepalived_version=1.3.5
 loadbalancer_version=HAProxy-${haproxy_version}_Keepalived-${keepalived_version}
@@ -17,15 +17,15 @@ kube_prometheus_version=0.13.0
 metrics_server_version=0.7.1
 dashboard_version=2.7.0
 metrics_scraper_version=1.0.8
-flannel_version=0.24.4
+flannel_version=0.25.1
 flannel_cni_plugin_version=1.4.0-flannel1
 calico_version=3.27.3
-helm_version=3.14.3
-istio_version=1.21.0
-contour_version=1.28.2
-contour_envoyproxy_version=1.29.2
+helm_version=3.14.4
+istio_version=1.21.1
+contour_version=1.28.3
+contour_envoyproxy_version=1.29.3
 elastic_cloud_version=2.12.1
-elastic_stack_version=8.13.1
+elastic_stack_version=8.13.2
 
 mv ${path}/kubernetes-playbook/version ${path}/kubernetes-playbook/v${kubernetes_version}
 mv ${path}/harbor-playbook/version ${path}/harbor-playbook/v${harbor_version}
