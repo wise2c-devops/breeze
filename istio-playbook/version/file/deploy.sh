7b4bdb672bdbfa56ae9c1ebd6d19223d8fb1dcf8
Alan Peng <peng.alan@gmail.com>
2024-04-21 09:38:25 -0400

    update deploy script

--- istio-playbook/version/file/deploy.sh
+++ istio-playbook/version/file/deploy.sh
@@ -38,6 +38,7 @@ sed -i "s,image: \"docker.io/grafana/,image: \"$MyImageRepositoryIP/$MyImageRepo
 sed -i "s,image: \"docker.io/jaegertracing/,image: \"$MyImageRepositoryIP/$MyImageRepositoryProject/,g" samples/addons/jaeger.yaml
 sed -i "s,image: \"ghcr.io/prometheus-operator/,image: \"$MyImageRepositoryIP/$MyImageRepositoryProject/,g" samples/addons/prometheus.yaml
 sed -i "s,image: \"jimmidyson/,image: \"$MyImageRepositoryIP/$MyImageRepositoryProject/,g" samples/addons/prometheus.yaml
+sed -i "s,image: \"prom/,image: \"$MyImageRepositoryIP/$MyImageRepositoryProject/,g" samples/addons/prometheus.yaml
 sed -i "s,- image: \"quay.io/kiali/,- image: \"$MyImageRepositoryIP/$MyImageRepositoryProject/,g" samples/addons/kiali.yaml
 sed -i "s,strategy: anonymous,strategy: token,g" samples/addons/kiali.yaml
 
