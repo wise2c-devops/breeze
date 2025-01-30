0c7ce535f729189846bedbc661b341c1743a4eb7
root <peng.alan@gmail.com>
2025-01-30 20:39:10 +0800

    Update components version

--- init.sh
+++ init.sh
@@ -11,21 +11,21 @@ crio_version=1.31.5
 haproxy_version=2.0.0
 keepalived_version=1.3.5
 loadbalancer_version=HAProxy-${haproxy_version}_Keepalived-${keepalived_version}
-prometheus_version=2.46.0
-prometheus_operator_version=0.67.1
-kube_prometheus_version=0.13.0
-metrics_server_version=0.7.1
+prometheus_version=3.1.0
+prometheus_operator_version=0.79.2
+kube_prometheus_version=0.14.0
+metrics_server_version=0.7.2
 dashboard_version=2.7.0
 metrics_scraper_version=1.0.8
-flannel_version=0.25.1
-flannel_cni_plugin_version=1.4.1-flannel1
-calico_version=3.27.3
-helm_version=3.14.4
-istio_version=1.21.1
-contour_version=1.28.3
-contour_envoyproxy_version=1.29.3
-elastic_cloud_version=2.12.1
-elastic_stack_version=8.13.2
+flannel_version=0.26.3
+flannel_cni_plugin_version=1.6.0-flannel1
+calico_version=3.29.1
+helm_version=3.17.0
+istio_version=1.24.2
+contour_version=1.30.2
+contour_envoyproxy_version=1.31.5
+elastic_cloud_version=2.16.1
+elastic_stack_version=8.17.1
 
 mv ${path}/kubernetes-playbook/version ${path}/kubernetes-playbook/v${kubernetes_version}
 mv ${path}/harbor-playbook/version ${path}/harbor-playbook/v${harbor_version}
