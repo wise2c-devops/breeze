23934613c6b92efe534347a43ad681d579289c11
root <peng.alan@gmail.com>
2025-04-30 22:55:47 +0800

    Upgrade components version

--- init.sh
+++ init.sh
@@ -17,15 +17,15 @@ kube_prometheus_version=0.14.0
 metrics_server_version=0.7.2
 dashboard_version=2.7.0
 metrics_scraper_version=1.0.8
-flannel_version=0.26.3
-flannel_cni_plugin_version=1.6.0-flannel1
-calico_version=3.29.1
-helm_version=3.17.0
-istio_version=1.23.4
-contour_version=1.30.2
-contour_envoyproxy_version=1.31.8
-elastic_cloud_version=2.16.1
-elastic_stack_version=8.17.1
+flannel_version=0.26.7
+flannel_cni_plugin_version=1.6.2-flannel1
+calico_version=3.29.3
+helm_version=3.17.3
+istio_version=1.25.2
+contour_version=1.30.3
+contour_envoyproxy_version=1.31.6
+elastic_cloud_version=3.0.0
+elastic_stack_version=9.0.0
 
 mv ${path}/kubernetes-playbook/version ${path}/kubernetes-playbook/v${kubernetes_version}
 mv ${path}/harbor-playbook/version ${path}/harbor-playbook/v${harbor_version}
