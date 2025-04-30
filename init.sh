8658c641ce18c37ea4ef5f99d92b2e5a4c4b7c3e
root <peng.alan@gmail.com>
2025-04-30 22:27:07 +0800

    Upgrade Kubernetes to v1.32.4

--- init.sh
+++ init.sh
@@ -4,10 +4,10 @@ set -e
 
 path=`dirname $0`
 
-kubernetes_version=1.32.1
+kubernetes_version=1.32.4
 harbor_version=2.12.2
 docker_version=26.1.3
-crio_version=1.32.1
+crio_version=1.32.4
 haproxy_version=2.0.0
 keepalived_version=1.3.5
 loadbalancer_version=HAProxy-${haproxy_version}_Keepalived-${keepalived_version}
